214413d282629fbd6acfc34deae31833c4ea94ea
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2017-08-25 13:19:53 +0200

    made book id a data class

--- library-service/src/main/kotlin/library/service/business/books/domain/types/BookId.kt
+++ library-service/src/main/kotlin/library/service/business/books/domain/types/BookId.kt
@@ -4,24 +4,12 @@ import library.service.business.books.domain.BookEntity
 import java.util.*
 
 /** The unique ID of a [BookEntity]. */
-class BookId(
+data class BookId(
         private val value: UUID
 ) {
 
     fun toUuid(): UUID = value
     override fun toString(): String = value.toString()
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-        other as BookId
-        if (value != other.value) return false
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return value.hashCode()
-    }
 
     companion object {
 
