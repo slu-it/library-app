a11a4949e0ce47c922ae50a6467199d841ac5fe6
Stefan Ludwig <stefan.ludwig@slu-it.de>
2017-08-26 21:43:07 +0200

    refactored test structure of BookEntityTest

--- library-service/src/test/kotlin/library/service/business/books/domain/BookEntityTest.kt
+++ library-service/src/test/kotlin/library/service/business/books/domain/BookEntityTest.kt
@@ -7,6 +7,7 @@ import library.service.business.books.exceptions.BookAlreadyBorrowedException
 import library.service.business.books.exceptions.BookAlreadyReturnedException
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import utils.UnitTest
 import java.time.OffsetDateTime
@@ -14,59 +15,48 @@ import java.time.OffsetDateTime
 @UnitTest
 internal class BookEntityTest {
 
-    @Test fun `book state is initialized as 'available'`() {
-        val id = BookId.generate()
-        val book = Book(Isbn13("0123456789012"), Title("Hello World"))
-        val bookEntity = BookEntity(id, book)
+    val book = Book(Isbn13("9780007507672"), Title("A Knight of the Seven Kingdoms"))
+    val bookId = BookId.generate()
 
-        assertThat(bookEntity.state).isEqualTo(Available)
+    val borrowed = Borrowed(Borrower("Duncan the Tall"), OffsetDateTime.now())
+
+    @Test fun `books are initialized as 'available'`() {
+        val minimalBook = BookEntity(bookId, book)
+        assertThat(minimalBook.state).isEqualTo(Available)
     }
 
-    @Test fun `book state can be changed to 'borrowed' if 'available'`() {
-        val id = BookId.generate()
-        val book = Book(Isbn13("0123456789012"), Title("Hello World"))
-        val bookEntity = BookEntity(id, book)
+    @Nested inner class `given an 'available' book` {
 
-        val by = Borrower("Someone")
-        val on = OffsetDateTime.now()
-        bookEntity.borrow(by, on)
+        val availableBook = BookEntity(bookId, book, Available)
 
-        assertThat(bookEntity.state).isEqualTo(Borrowed(by, on))
+        @Test fun `it can be 'borrowed'`() {
+            availableBook.borrow(borrowed.by, borrowed.on)
+            assertThat(availableBook.state).isEqualTo(borrowed)
+        }
+
+        @Test fun `trying to return it will throw an exception`() {
+            assertThrows(BookAlreadyReturnedException::class.java, {
+                availableBook.`return`()
+            })
+        }
+
     }
 
-    @Test fun `book state cannot be changed to 'borrowed' if already 'borrowed'`() {
-        val id = BookId.generate()
-        val book = Book(Isbn13("0123456789012"), Title("Hello World"))
-        val bookEntity = BookEntity(id, book)
+    @Nested inner class `given a 'borrowed' book` {
 
-        val by = Borrower("Someone")
-        val on = OffsetDateTime.now()
-        bookEntity.borrow(by, on)
+        val borrowedBook = BookEntity(bookId, book, borrowed)
 
-        assertThrows(BookAlreadyBorrowedException::class.java, {
-            bookEntity.borrow(by, on)
-        })
-    }
+        @Test fun `it can be returned in order to make it 'available' again`() {
+            borrowedBook.`return`()
+            assertThat(borrowedBook.state).isEqualTo(Available)
+        }
 
-    @Test fun `book state can be changed to 'available' if 'borrowed'`() {
-        val id = BookId.generate()
-        val book = Book(Isbn13("0123456789012"), Title("Hello World"))
-        val bookEntity = BookEntity(id, book)
+        @Test fun `trying to borrow it will throw an exception`() {
+            assertThrows(BookAlreadyBorrowedException::class.java, {
+                borrowedBook.borrow(borrowed.by, borrowed.on)
+            })
+        }
 
-        bookEntity.borrow(Borrower("Someone"), OffsetDateTime.now())
-        bookEntity.`return`()
-
-        assertThat(bookEntity.state).isEqualTo(Available)
-    }
-
-    @Test fun `book state cannot be changed to 'available' if already 'available'`() {
-        val id = BookId.generate()
-        val book = Book(Isbn13("0123456789012"), Title("Hello World"))
-        val bookEntity = BookEntity(id, book)
-
-        assertThrows(BookAlreadyReturnedException::class.java, {
-            bookEntity.`return`()
-        })
     }
 
 }
