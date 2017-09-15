e82514f50a01716d3d2faf58063384d08a729d0c
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2017-09-15 18:48:12 +0200

    added redirect controller

--- library-service/src/main/kotlin/library/service/Application.kt
+++ library-service/src/main/kotlin/library/service/Application.kt
@@ -3,13 +3,27 @@ package library.service
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.servlet.view.RedirectView
 import java.time.Clock
+
 
 @SpringBootApplication
 class Application {
 
     @Bean
     fun clock(): Clock = Clock.systemUTC()
+
+    @Controller
+    class RedirectController {
+
+        @GetMapping("/")
+        fun redirectIndexToDocumentation(): RedirectView {
+            return RedirectView("/docs/index.html")
+        }
+
+    }
 
 }
 
