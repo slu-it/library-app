5bd0599c95d35e5f12c7e738c89f2a3e82071480
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2017-12-28 19:33:55 +0100

    corrected documentation mistakes

--- library-service/src/main/kotlin/library/service/business/events/EventDispatcher.kt
+++ library-service/src/main/kotlin/library/service/business/events/EventDispatcher.kt
@@ -2,15 +2,15 @@ package library.service.business.events
 
 /**
  * Interface defining all methods which need to be implemented by an event dispatcher.
- * These are used to inform other bounded contexts about domain events.
+ * These are used to inform other bounded contexts about [DomainEvent] occurrences.
  */
 interface EventDispatcher<in T: DomainEvent> {
 
     /**
-     * Dispatches the given event to be processed by other bounded
-     * contexts interested in it.
+     * Dispatches the given [DomainEvent] to be processed by other bounded
+     * contexts which might be interested in it.
      *
-     * @param event the domain event to dispatch
+     * @param event the [DomainEvent] to dispatch
      */
     fun dispatch(event: T)
 
