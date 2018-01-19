43644204f35f0a4ed15908f71d28b7626795501d
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2018-01-19 16:43:58 +0100

    book added events are now ignoring  unknown properties

--- library-enrichment/src/main/kotlin/library/enrichment/core/BookAddedEvent.kt
+++ library-enrichment/src/main/kotlin/library/enrichment/core/BookAddedEvent.kt
@@ -1,5 +1,8 @@
 package library.enrichment.core
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class BookAddedEvent(
         val id: String,
         val bookId: String,
---
+++ library-enrichment/src/test/kotlin/library/enrichment/core/BookAddedEventTest.kt
@@ -0,0 +1,41 @@
+package library.enrichment.core
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.util.*
+
+
+internal class BookAddedEventTest {
+
+    val objectMapper = ObjectMapper().apply { findAndRegisterModules() }
+
+    @Test fun `can be de-serialized from JSON`() {
+        val id = UUID.randomUUID()
+        val bookId = UUID.randomUUID()
+        val json = """
+            {
+              "id": "$id",
+              "bookId": "$bookId",
+              "isbn": "1234567890123"
+            }
+            """
+        val event = objectMapper.readValue(json, BookAddedEvent::class.java)
+        assertThat(event.id).isEqualTo(id.toString())
+        assertThat(event.bookId).isEqualTo(bookId.toString())
+        assertThat(event.isbn).isEqualTo("1234567890123")
+    }
+
+    @Test fun `unknown properties are ignored`() {
+        val json = """
+            {
+              "id": "${UUID.randomUUID()}",
+              "bookId": "${UUID.randomUUID()}",
+              "isbn": "1234567890123",
+              "foo": "bar"
+            }
+            """
+        objectMapper.readValue(json, BookAddedEvent::class.java)
+    }
+
+}
