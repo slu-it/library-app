63a9ffa00ad452a2235b13f1a1642a3c1404e582
Antoniya Atanasova <antoniya.atanasova@novatec-gmbh.de>
2018-08-08 17:12:47 +0200

    Enabled spring actuator, added custom security configuraion.

--- library-integration-slack/src/main/kotlin/library/integration/slack/SlackIntegrationApplication.kt
+++ library-integration-slack/src/main/kotlin/library/integration/slack/SlackIntegrationApplication.kt
@@ -2,10 +2,11 @@ package library.integration.slack
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
 class SlackIntegrationApplication {
     @Bean
     fun objectMapper(): ObjectMapper = ObjectMapper().apply {
