3ecc6acd9466784b75dc5924d1d9c8ea9b7997e2
Antoniya Atanasova <antoniya.atanasova@novatec-gmbh.de>
2018-09-14 16:04:11 +0200

    Updated contract messaging test with the new Pact Junit ext for messaging.

--- library-integration-slack/src/test/kotlin/library/integration/slack/messaging/BookAddedMessageContractTest.kt
+++ library-integration-slack/src/test/kotlin/library/integration/slack/messaging/BookAddedMessageContractTest.kt
@@ -1,8 +1,13 @@
 package library.integration.slack.messaging
 
 import au.com.dius.pact.consumer.MessagePactBuilder
+import au.com.dius.pact.consumer.Pact
 import au.com.dius.pact.consumer.dsl.PactDslJsonBody
-import au.com.dius.pact.model.PactSpecVersion
+import au.com.dius.pact.consumer.junit5.PactConsumerTestExt
+import au.com.dius.pact.consumer.junit5.PactTestFor
+import au.com.dius.pact.consumer.junit5.ProviderType
+import au.com.dius.pact.model.v3.messaging.Message
+import au.com.dius.pact.model.v3.messaging.MessagePact
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.nhaarman.mockitokotlin2.check
 import com.nhaarman.mockitokotlin2.mock
@@ -11,6 +16,8 @@ import library.integration.slack.core.BookAddedEventHandler
 import library.integration.slack.core.Slack
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.amqp.core.MessageProperties
 import org.springframework.amqp.rabbit.connection.ConnectionFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -21,12 +28,12 @@ import org.springframework.test.context.ContextConfiguration
 import utils.classification.ContractTest
 import utils.extensions.EnableSpringExtension
 import utils.messaging.BookEventsMessageProvider.Companion.bookAddedEvent
-import utils.messaging.toMessageConverter
 import utils.objectMapper
 
 @SpringBootTest
 @ContractTest
 @EnableSpringExtension
+@ExtendWith(PactConsumerTestExt::class)
 @ContextConfiguration(classes = [BookAddedMessageContractTest.TestConfiguration::class])
 class BookAddedMessageContractTest {
 
@@ -60,30 +67,33 @@ class BookAddedMessageContractTest {
     lateinit var bookAddedMessageConsumer: BookAddedMessageConsumer
 
 
+    @Pact(provider = PROVIDER, consumer = CONSUMER)
+    fun createPact(builder: MessagePactBuilder): MessagePact {
+        return builder
+            .given("new book has been added to the library")
+            .expectsToReceive("event for book-added is received")
+            .withContent(
+                PactDslJsonBody()
+                    .stringType("isbn", bookAddedEvent.isbn)
+                    .stringType("title", bookAddedEvent.title)
+            )
+            .toPact()
+    }
+
     @Test
-    fun `book added event contract test`() {
+    @PactTestFor(pactMethod = "createPact", providerType = ProviderType.ASYNCH)
+    fun `book added event contract test`(messages: List<Message>) {
+        //gets the message from the pact mock server
+        val messageConverted =
+            org.springframework.amqp.core.Message(messages.get(0).contentsAsBytes(), MessageProperties())
 
-        val pact = MessagePactBuilder
-                .consumer(CONSUMER)
-                .hasPactWith(PROVIDER)
-                .given("new book has been added to the library")
-                .expectsToReceive("event for book-added is received")
-                .withContent(
-                        PactDslJsonBody()
-                                .stringType("isbn", bookAddedEvent.isbn)
-                                .stringType("title", bookAddedEvent.title))
-                .toPact()
+        bookAddedMessageConsumer.onMessage(messageConverted) // message is sent on the consumer
 
-        pact.messages.forEach {
-            val message = toMessageConverter(bookAddedEvent, objectMapper);
-            bookAddedMessageConsumer.onMessage(message)
-        }
-
-        verify(bookAddedEventHandler).handleBookAdded(check {
-            assertThat(it.isbn).isEqualTo(bookAddedEvent.isbn)
-            assertThat(it.title).isEqualTo(bookAddedEvent.title)
-        })
-
-        pact.write(PACT_DIR, PactSpecVersion.V3)
+        verify(bookAddedEventHandler).handleBookAdded(
+            check {
+                assertThat(it.isbn).isEqualTo(bookAddedEvent.isbn)
+                assertThat(it.title).isEqualTo(bookAddedEvent.title)
+            }
+        )
     }
 }
