e3ceade16b16e82c89706eb2cac7388a059a9561
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2019-10-04 14:13:23 +0200

    library-service: upgraded to PACT 4.0 and removed custom messaging contract tests

--- library-service/src/test/kotlin/pact/MessageContractTest.kt
+++ library-service/src/test/kotlin/pact/MessageContractTest.kt
@@ -1,43 +1,59 @@
 package pact
 
+import au.com.dius.pact.provider.PactVerifyProvider
+import au.com.dius.pact.provider.junit.Provider
+import au.com.dius.pact.provider.junit.VerificationReports
+import au.com.dius.pact.provider.junit.loader.PactFolder
+import au.com.dius.pact.provider.junit5.AmpqTestTarget
+import au.com.dius.pact.provider.junit5.PactVerificationContext
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider
 import com.fasterxml.jackson.databind.ObjectMapper
 import library.service.business.books.domain.BookRecord
 import library.service.business.books.domain.events.BookAdded
 import library.service.business.books.domain.types.BookId
 import library.service.messaging.MessagingConfiguration
-import org.junit.jupiter.api.TestFactory
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
+import org.junit.jupiter.api.TestTemplate
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.amqp.core.MessageProperties
-import org.testit.pact.provider.junit.PactTestFactory
-import org.testit.pact.provider.message.ActualMessage
-import org.testit.pact.provider.message.MessagePacts
-import org.testit.pact.provider.message.MessageProducer
-import org.testit.pact.provider.sources.LocalFiles
 import utils.Books
 import utils.classification.ContractTest
 import java.time.OffsetDateTime
 import java.util.*
 
 @ContractTest
+@Provider("library-service")
+@PactFolder("src/test/pacts/message")
+@VerificationReports("console")
+@TestInstance(PER_CLASS) // PACT needs this ... for some reason ...
 class MessageContractTest {
 
     val configuration = MessagingConfiguration()
     val objectMapper = ObjectMapper().apply { findAndRegisterModules() }
     val messageConverter = configuration.messageConverter(objectMapper)
 
-    val pacts = MessagePacts(LocalFiles("src/test/pacts/message"), "library-service")
+    @BeforeEach
+    fun setTarget(context: PactVerificationContext) {
+        context.target = AmpqTestTarget(listOf("pact"))
+    }
 
-    @TestFactory fun `library-enrichment consumer contract tests`() =
-        PactTestFactory.createTests(pacts, "library-enrichment", this)
+    @TestTemplate
+    @ExtendWith(PactVerificationInvocationContextProvider::class)
+    fun pactVerificationTestTemplate(context: PactVerificationContext) {
+        context.verifyInteraction()
+    }
 
-    @MessageProducer("'The Martian' was added event")
-    fun `verify The Martian was added event`(): ActualMessage {
+    @PactVerifyProvider("'The Martian' was added event")
+    fun `verify 'The Martian' was added event`(): String {
         val event = BookAdded(
-            id = UUID.randomUUID(),
-            timestamp = OffsetDateTime.now(),
-            bookRecord = BookRecord(BookId.generate(), Books.THE_MARTIAN)
+                id = UUID.randomUUID(),
+                timestamp = OffsetDateTime.now(),
+                bookRecord = BookRecord(BookId.generate(), Books.THE_MARTIAN)
         )
         val message = messageConverter.toMessage(event, MessageProperties())
-        return ActualMessage(message.body)
+        return String(message.body)
     }
 
 }
