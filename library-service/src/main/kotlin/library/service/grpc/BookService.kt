c91950a48d2f23c56489e69677c372b08cccf7da
Antoniya Atanasova <antoniya.atanasova@novatec-gmbh.de>
2020-10-01 11:13:27 +0200

    Added book grpc service implementation, optimised logging.

--- library-service/src/main/kotlin/library/service/grpc/BookService.kt
+++ library-service/src/main/kotlin/library/service/grpc/BookService.kt
@@ -5,33 +5,36 @@ import library.service.business.books.domain.composites.Book
 import library.service.business.books.domain.types.Isbn13
 import library.service.business.books.domain.types.Title
 import library.service.grpc.CreateBookGrpcKt.CreateBookCoroutineImplBase
+import library.service.logging.logger
+import library.service.security.SecurityCommons
 import org.springframework.stereotype.Component
 
 @Component
 class BookService(
-    private val collection: BookCollection
+    private val collection: BookCollection,
+    private val mapper: CreateBookResponseMapper,
+    private val securityCommons: SecurityCommons
 ) : CreateBookCoroutineImplBase() {
 
+    private val log = BookService::class.logger
+
     override suspend fun createBook(request: CreateBookRequest): CreateBookResponse {
-        println("Received book with ${request.title} and ${request.isbn}") //TODO: replace with log
-        val book = Book(
-            isbn = Isbn13.parse(request.isbn),
-            title = Title(request.title),
-            authors = emptyList(),
-            numberOfPages = null
-        )
-        val bookRecord = collection.addBook(book)
-        val response = CreateBookResponse
-            .newBuilder()
-            .apply {
-                this.isbn = bookRecord.book.isbn.toString()
-                this.title = bookRecord.book.title.toString()
-                // this.authors = emptyArray<String>()
-                this.numberOfPages = bookRecord.book.numberOfPages!!
-                this.borrowed = null
-            }
-            .build()
-        println("Sending response with $response")
-        return response
+        try {
+            log.info("Received book with ${request.title} and ${request.isbn}")
+            val book = Book(
+                isbn = Isbn13.parse(request.isbn),
+                title = Title(request.title),
+                authors = emptyList(),
+                numberOfPages = null
+            )
+            val bookRecord = securityCommons.executeOperationAsCurator { collection.addBook(book) }
+            val response = mapper.toCreateBookResponse(bookRecord)
+            log.info("Sending response with $response")
+            return response
+        } catch (e: Exception) {
+            log.error("Grpc Response could not be sent due to error = [${e.message}]")
+            log.debug("${e.printStackTrace()}")
+            throw e
+        }
     }
 }
